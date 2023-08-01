da52a731b0e2efb6ded8a434054d5e5c56ca8c66
Kingdom <kingdom@Kingdoms-MacBook-Air.local>
2023-08-01 16:07:11 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    // Change code below this line
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+    // Change code above this line
+  }
+  // Change code below this line
+  increment(){
+    this.setState(state => {
+      return{
+      count: state.count+1
+      }
+    });
+  }
+
+  decrement(){
+    this.setState(state => {
+      return{
+        count:state.count-1
+        }
+      });
+
+  }
+
+  reset(){
+    this.setState({count: 0});
+  }
+  // Change code above this line
+  render() {
+    return (
+
+    <div className='App-header'>
+      <h2>This is my simple counter</h2>
+      <div className='App'>
+        <button className='btn' onClick={this.increment}>Increment!</button>
+        <button className='btn' onClick={this.decrement}>Decrement!</button>
+        <button className='btn' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
     </div>
-  );
-}
+    );
+  }
+};
+
 
 export default App;
